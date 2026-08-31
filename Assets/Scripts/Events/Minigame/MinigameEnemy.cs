a5e976e020caff4cbf4ae6d42f3278389a1cec9f
FluffyLuke <kacper.lukstaedt@gmail.com>
2026-08-31 14:37:23 +0200

    update: monster model from top

--- Assets/Scripts/Events/Minigame/MinigameEnemy.cs
+++ Assets/Scripts/Events/Minigame/MinigameEnemy.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.Events;
 using System.Collections;
+using UnityEngine.UIElements;
 public class MinigameEnemy : MonoBehaviour {
     [Header("Sounds")]
     public float volume = 1;
@@ -11,11 +12,13 @@ public class MinigameEnemy : MonoBehaviour {
     public string attackSound2ID;
     public AudioSource source;
     [Header("Speed")]
+    public float waitBeforeHideTop = 1f;
     public float showSpeed = 1;
     public float hideSpeed = 2;
     public float timeToKill = 2;
     public float timePenalty = 1;
     [Header("Animation")]
+    public bool crawler = false;
     public Animator animator;
     public Transform initPos;
     public Transform showPos;
@@ -27,7 +30,13 @@ public class MinigameEnemy : MonoBehaviour {
     private float timeLeft;
 
     public void Show() {
-        animator.Play("Monster_Walk");
+        animator.SetBool("Crawler", crawler);
+        if (crawler) {
+            animator.Play("Monster_IdleCrawl");
+        } else {
+            animator.Play("Monster_Walk");
+        }
+
         Debug.Log("Showing enemy...");
         transform.position = initPos.position;
         transform.rotation = showPos.rotation;
@@ -58,10 +67,27 @@ public class MinigameEnemy : MonoBehaviour {
     }
 
     public void Hide() {
-        animator.Play("Monster_Walk_Backwards");
+        if (crawler) {
+            StartCoroutine(crawlBack(waitBeforeHideTop));
+            return;
+        } else {
+            SoundManager.instance.PlayOneShot(hideSoundID, gameObject, out SoundHandle handle, source);
+            animator.Play("Monster_Walk_Backwards");
+        }
         Debug.Log("Hiding enemy...");
 
+        StopAllCoroutines();
+        onHide.Invoke();
+        transform
+            .DOMove(initPos.position, hideSpeed)
+            .OnComplete(() => Destroy(gameObject));
+    }
+    private IEnumerator crawlBack(float timeSecs) {
+        yield return new WaitForSeconds(timeSecs);
         SoundManager.instance.PlayOneShot(hideSoundID, gameObject, out SoundHandle handle, source);
+        
+        animator.SetTrigger("CrawlAway");
+        Debug.Log("Hiding enemy...");
 
         StopAllCoroutines();
         onHide.Invoke();
@@ -72,6 +98,14 @@ public class MinigameEnemy : MonoBehaviour {
     public void Attack() {
         transform.rotation = killPos.rotation;
         transform.position = killPos.position;
+
+        if (crawler) {
+            animator.SetTrigger("Jump");
+            transform.DOScale(2, 1);
+        } else {
+            // No attack animation
+        }
+
         SoundManager.instance.PlayOneShot(attackSoundID, gameObject, out SoundHandle handle, source);
         SoundManager.instance.PlayOneShot(attackSound2ID, gameObject, out handle, source);
         onAttack.Invoke();
